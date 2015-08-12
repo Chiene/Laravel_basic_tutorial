fc251412c2d92a761dfd13637dda8d0db4ceb437
Jia-ching Jian <sagiters1128@gmail.com>
2015-08-12 12:33:42 +0800

    add blade template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel Basic Tutorial</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <h1>Laravel 5 Tutorial</h1>
-                <hr>
-            </div>
-             <div class="row">
-                <h3>Outline</h3>
-                <ul>
-                    <li><h4>Routes</h4></li>
-                    <li><h4>Create a messageboard page and resource controller</h4></li>
-                    <li><h4>Create a database table with migration</h4></li>
-                    <li><h4>Leave a message</h4></li>
-                </ul>
-                
-            </div>
-        </div>
-    </body>
-</html>
+@extends('layout')
+@section("contents")
+     <div class="row">
+        <h3>Outline</h3>
+        <ul>
+            <li><h4>Routes</h4></li>
+            <li><h4>Blade templates</h4></li>
+            <li><h4>Create a messageboard page and resource controller</h4></li>
+            <li><h4>Create a database table with migration</h4></li>
+            <li><h4>Leave a message</h4></li>
+        </ul>
+    </div>
+@stop
